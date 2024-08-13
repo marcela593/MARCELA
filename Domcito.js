ac6b6593d8e2858625beedc23d9c945ad6c8a930
marcela593 <m.melara3005@gmail.com>
2024-08-13 10:17:13 -0600

    Add files via upload

---
+++ Domcito.js
@@ -0,0 +1,10 @@
+function calcular()
+{
+    let nombre =document.getElementById('name').Value;
+    let edad =parseInt(document.getElementById('edad1').Value);
+    let aumento;
+    aumento=edad+10;
+    alert("su edad aumentada es"+aumento)
+
+
+}
